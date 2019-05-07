468e9ea744a602d0b0931c4e7f963d43b5f193b6
Scott Reese <scott_reese@pm.me>
2019-05-07 10:23:10 -0400

    assessment completed and working to rqs

--- mimic.py
+++ mimic.py
@@ -46,23 +46,49 @@ columns, so the output looks better.
 import random
 import sys
 
+def gen_word_list(file):
+    arr = []
+    with open(file, 'r') as f:
+        line = f.readline()
+        while len(line) > 0:
+            arr += line.split()
+            line = f.readline()
+    return arr
 
-def mimic_dict(filename):
+def remove_asterisks(arr):
+    while '*' in arr:
+        del arr[arr.index('*')]
+    return arr
+
+
+def mimic_dict(file):
     """Returns mimic dict mapping each word to list of words which follow it."""
-    # +++your code here+++
-    raise NotImplementedError("Get to Work!")
+    result = {}
+    arr = remove_asterisks(gen_word_list(file))
+    
+    for index, word in enumerate(arr):
+        lowercase_word = word.lower()
+        next_index = index + 1
+        if lowercase_word in result and next_index != len(arr):
+            result[lowercase_word] += [arr[next_index].lower()]
+        elif next_index != len(arr):
+            result.update({ lowercase_word : [arr[next_index].lower()] })
+
+    return result
 
 
 def print_mimic(mimic_dict, word):
     """Given mimic dict and start word, prints 200 random words."""
-    # +++your code here+++
-    raise NotImplementedError("Get to Work!")
+    result = ""
+    for i in range(200):
+        result = result + " " + random.choice(mimic_dict[random.choice(mimic_dict.keys())])
+    print(result)
 
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
     if len(sys.argv) != 2:
-        print 'usage: python mimic.py file-to-read'
+        print('usage: python mimic.py file-to-read')
         sys.exit(1)
 
     d = mimic_dict(sys.argv[1])
